feab888e9b3839c8154174d8acca1bdd105cda71
thatGui <gui@thatgui.com>
2022-03-17 17:08:17 +0000

    style added

--- src/components/Contactdetails/Contactdetails.jsx
+++ src/components/Contactdetails/Contactdetails.jsx
@@ -9,41 +9,62 @@ import { FaEdit } from "react-icons/fa";
 function Contactdetails(props) {
   
     return (
-    <div className='detailspane'>
-      <div className="contact">
-        <img src={props.ac.avatar} alt="profile pic" />
-        <div className="contdeets">
-          <h4>Job Title</h4>
-          <p>{props.ac.jobTitle}</p>
-          <h4>Email</h4>
-          <p>{props.ac.email}</p>
-          <h4>Tel</h4>
-          <p>{props.ac.telephone}</p>
-        </div>
 
-          <div className="namesec">
-          <h2>{props.ac.firstName}</h2>
+    <div className='detailspane'>
+
+      <div className="namepic">
+          <img src={props.ac.avatar} alt="profile pic" />
+          <div className='namepicds'>
+          <h4>Name</h4>
+          <p>{props.ac.firstName}</p>
+          </div>
+          <div  className='namepicds'>
+          <h4>Surname</h4>
           <p>{props.ac.lastName}</p>
           </div>
-        </div>
-        <div className="detailsbox">
-          
-          <div className="contact-info">
-          <h4>Timezone</h4>
-          <p>{props.ac.timezone}</p>
+      </div>
+
+      <div className="contdeets">
+          <div  className='namepicds'>
+          <h4>Email</h4>
+          <p>{props.ac.email}</p>
+          </div>
+          <div  className='namepicds'>
+          <h4>Phone Number</h4>
+          <p>{props.ac.telephone}</p>
+          </div>
+          <div  className='namepicds'>
           <h4>Location</h4>
           <p>{props.ac.location}</p>
+          </div>
+      </div>
+
+        
+        <div className="contdeets">
+          <div  className='namepicds'>
+          <h4>Job Title</h4>
+          <p>{props.ac.jobTitle}</p>
+          </div>
+          <div  className='namepicds'>
+          <h4>Timezone</h4>
+          <p>{props.ac.timezone}</p>
+          </div>
+          <div  className='namepicds'>
           <h4>Accounts</h4>
           <p>{props.ac.accounts}</p>
+          </div>
+          <div  className='namepicds'>
           <h4>Deals</h4>
           <p>{props.ac.deals}</p>
-          <h4>User</h4>
-          <p>{props.ac.user}</p>
           </div>
+         {/*  <h4>User</h4>
+          <p>{props.ac.user}</p> */}
+          
         </div>
-        <div className="edit-btn">
+        
         <FaEdit onClick={ () => props.handleDisplayState('edit')} className='ceitem' ce={props.ac._id} />
-        </div>
+        
+
     </div>
   )
 }

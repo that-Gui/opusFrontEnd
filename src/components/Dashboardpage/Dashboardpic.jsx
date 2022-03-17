b46d3c37f24ab3af9b7e0ec38bedaedfce0de52e
thatGui <gui@thatgui.com>
2022-03-17 10:11:21 +0000

    pic alt again

--- src/components/Dashboardpage/Dashboardpic.jsx
+++ src/components/Dashboardpage/Dashboardpic.jsx
@@ -6,7 +6,7 @@ function Dashboardpic() {
     <div className='container'>
 
         <div className="dashpic">
-            <img src="./Dashboardv1.png" alt="possible variant of dashboard" />
+            <img src="https://i.imgur.com/CoxADQJ.png" alt="possible variant of dashboard" />
         </div>
 
     </div>
